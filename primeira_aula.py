623f27593b6bb816a840e17aeba1edf89778e4c8
labsfiap <celsoferrersingh@icloud.com>
2025-03-17 22:48:00 -0300

    Primeiro programa em Python.

--- primeira_aula.py
+++ primeira_aula.py
@@ -1,1 +1,21 @@
-print("Hello world!")
+ # exercicio um
+print("Celso Fernando Ferrer Singh\n")
+
+# exercicio dois
+# a=3
+# b=5
+# c=10
+# print(a*2*3*b)
+
+num1= float(input("Numero 1 ="))
+num2= float(input("Numero 2 ="))
+num3= float(input("Numero 3 ="))
+
+soma= num1+num2+num3
+print(f"Soma={soma:.2f}")
+media= (soma)/3
+print(f"Média={media:.2f}")
+
+#exercicio 3
+#print(a+b+c)
+
